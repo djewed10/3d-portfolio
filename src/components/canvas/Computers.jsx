5ee1e2c8010e4d468f265b685a14d1f4b041707b
djewed10 <153443944+djewed10@users.noreply.github.com>
2024-04-16 19:10:47 +0100

    Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -63,17 +63,15 @@ const ComputersCanvas = () => {
       //dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={CanvasLoader}>
-        <OrbitControls
+    >  <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
+<Suspense fallback={CanvasLoader}>
         <Computers isMobile={isMobile} />
+        <Preload all />
       </Suspense>
-
-      <Preload all />
     </Canvas>
   );
 };
